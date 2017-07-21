a33394ccaaedbcd5cf8e988e070265534ff670d8
Christine Chang <xtnchang@gmail.com>
2017-07-21 08:53:07 -0700

    Refactor getCityAirQuality closure
    check airQuality != nil in guard statement (previously in if statement)
    to cut back one level of nesting

--- Runners' Air Check/AirScoreViewController.swift
+++ Runners' Air Check/AirScoreViewController.swift
@@ -51,102 +51,98 @@ class AirScoreViewController: UIViewController {
                 return
             }
             
-            if airQuality != nil {
-                
-                print(airQuality ?? "")
-                
-                if airQuality! < 51 {
-                    print("Good: green.")
-                    
-                    DispatchQueue.main.async {
-                        self.airScoreCircle.isHidden = false
-                        self.airScoreLabel.text = String(describing: airQuality!)
-                        self.cityLabel.isHidden = false
-                        self.cityLabel.text = self.cityNameTapped
-                        self.messageLabel.text = "The air’s great! \nGo run your socks off!!"
-                        self.treadmillImageView.isHidden = true
-                        self.trailImageView.isHidden = false
-                        self.airScoreCircle.color = UIColor(red: 0.6824, green: 1, blue: 0.4863, alpha: 1.0)
-                        self.airScoreCircle.setNeedsDisplay()
-                    }
-                } else if airQuality! < 100 {
-                    print("Moderate: yellow")
-                    
-                    DispatchQueue.main.async {
-                        self.airScoreCircle.isHidden = false
-                        self.airScoreLabel.text = String(describing: airQuality!)
-                        self.cityLabel.isHidden = false
-                        self.cityLabel.text = self.cityNameTapped
-                        self.messageLabel.text = "The air’s pretty good. \nGet in those miles!"
-                        self.treadmillImageView.isHidden = true
-                        self.trailImageView.isHidden = false
-                        self.airScoreCircle.color = UIColor(red: 1, green: 0.9647, blue: 0.4863, alpha: 1.0)
-                        self.airScoreCircle.setNeedsDisplay()
-                    }
-                } else if airQuality! < 150 {
-                    print("Unhealthy for sensitive groups: orange")
-                    
-                    DispatchQueue.main.async {
-                        self.airScoreCircle.isHidden = false
-                        self.airScoreLabel.text = String(describing: airQuality!)
-                        self.cityLabel.isHidden = false
-                        self.cityLabel.text = self.cityNameTapped
-                        self.messageLabel.text = "The air’s okay. Take it easy out there, especially if you have asthma!"
-                        self.treadmillImageView.isHidden = true
-                        self.trailImageView.isHidden = false
-                        self.airScoreCircle.color = UIColor(red: 1, green: 0.7843, blue: 0.4863, alpha: 1.0)
-                        self.airScoreCircle.setNeedsDisplay()
-                    }
-                } else if airQuality! < 200 {
-                    print("Unhealthy: red")
-                    
-                    DispatchQueue.main.async {
-                        self.airScoreCircle.isHidden = false
-                        self.airScoreLabel.text = String(describing: airQuality!)
-                        self.cityLabel.isHidden = false
-                        self.cityLabel.text = self.cityNameTapped
-                        self.messageLabel.text = "The air’s not great. Better stick to the treadmill!"
-                        self.trailImageView.isHidden = true
-                        self.treadmillImageView.isHidden = false
-                        self.airScoreCircle.color = UIColor(red: 0.9569, green: 0.4667, blue: 0.4667, alpha: 1.0)
-                        self.airScoreCircle.setNeedsDisplay()
-                    }
-                } else if airQuality! < 300 {
-                    print("Very unhealthy: purple")
-                    
-                    DispatchQueue.main.async {
-                        self.airScoreCircle.isHidden = false
-                        self.airScoreLabel.text = String(describing: airQuality!)
-                        self.cityLabel.isHidden = false
-                        self.cityLabel.text = self.cityNameTapped
-                        self.messageLabel.text = "The air’s pretty polluted. Better stick to the treadmill, and avoid going out!"
-                        self.trailImageView.isHidden = true
-                        self.treadmillImageView.isHidden = false
-                        self.airScoreCircle.color = UIColor(red: 0.651, green: 0.5059, blue: 0.7569, alpha: 1.0)
-                        self.airScoreCircle.setNeedsDisplay()
-                    }
-                } else if airQuality! >= 300 {
-                    print("Hazardous: brown")
-                    
-                    DispatchQueue.main.async {
-                        self.airScoreCircle.isHidden = false
-                        self.airScoreLabel.text = String(describing: airQuality!)
-                        self.cityLabel.isHidden = false
-                        self.cityLabel.text = self.cityNameTapped
-                        self.messageLabel.text = "The air is dangerously polluted. Better stick to the treadmill, and keep your windows shut!"
-                        self.trailImageView.isHidden = true
-                        self.treadmillImageView.isHidden = false
-                        self.airScoreCircle.color = UIColor(red: 0.6196, green: 0.5333, blue: 0.4118, alpha: 1.0)
-                        self.airScoreCircle.setNeedsDisplay()
-                    }
-                }
-                
-            } else {
-                
+            guard (airQuality != nil) else {
                 DispatchQueue.main.async {
                     self.airScoreCircle.isHidden = true
                     self.cityLabel.isHidden = true
                     self.messageLabel.text = "Sorry, there is currently no data for \(self.cityNameTapped!). Go for a run anyway!"
+                }
+                return
+            }
+            
+            if airQuality! < 51 {
+                print("Good: green.")
+                
+                DispatchQueue.main.async {
+                    self.airScoreCircle.isHidden = false
+                    self.airScoreLabel.text = String(describing: airQuality!)
+                    self.cityLabel.isHidden = false
+                    self.cityLabel.text = self.cityNameTapped
+                    self.messageLabel.text = "The air’s great! \nGo run your socks off!!"
+                    self.treadmillImageView.isHidden = true
+                    self.trailImageView.isHidden = false
+                    self.airScoreCircle.color = UIColor(red: 0.6824, green: 1, blue: 0.4863, alpha: 1.0)
+                    self.airScoreCircle.setNeedsDisplay()
+                }
+            } else if airQuality! < 100 {
+                print("Moderate: yellow")
+                
+                DispatchQueue.main.async {
+                    self.airScoreCircle.isHidden = false
+                    self.airScoreLabel.text = String(describing: airQuality!)
+                    self.cityLabel.isHidden = false
+                    self.cityLabel.text = self.cityNameTapped
+                    self.messageLabel.text = "The air’s pretty good. \nGet in those miles!"
+                    self.treadmillImageView.isHidden = true
+                    self.trailImageView.isHidden = false
+                    self.airScoreCircle.color = UIColor(red: 1, green: 0.9647, blue: 0.4863, alpha: 1.0)
+                    self.airScoreCircle.setNeedsDisplay()
+                }
+            } else if airQuality! < 150 {
+                print("Unhealthy for sensitive groups: orange")
+                
+                DispatchQueue.main.async {
+                    self.airScoreCircle.isHidden = false
+                    self.airScoreLabel.text = String(describing: airQuality!)
+                    self.cityLabel.isHidden = false
+                    self.cityLabel.text = self.cityNameTapped
+                    self.messageLabel.text = "The air’s okay. Take it easy out there, especially if you have asthma!"
+                    self.treadmillImageView.isHidden = true
+                    self.trailImageView.isHidden = false
+                    self.airScoreCircle.color = UIColor(red: 1, green: 0.7843, blue: 0.4863, alpha: 1.0)
+                    self.airScoreCircle.setNeedsDisplay()
+                }
+            } else if airQuality! < 200 {
+                print("Unhealthy: red")
+                
+                DispatchQueue.main.async {
+                    self.airScoreCircle.isHidden = false
+                    self.airScoreLabel.text = String(describing: airQuality!)
+                    self.cityLabel.isHidden = false
+                    self.cityLabel.text = self.cityNameTapped
+                    self.messageLabel.text = "The air’s not great. Better stick to the treadmill!"
+                    self.trailImageView.isHidden = true
+                    self.treadmillImageView.isHidden = false
+                    self.airScoreCircle.color = UIColor(red: 0.9569, green: 0.4667, blue: 0.4667, alpha: 1.0)
+                    self.airScoreCircle.setNeedsDisplay()
+                }
+            } else if airQuality! < 300 {
+                print("Very unhealthy: purple")
+                
+                DispatchQueue.main.async {
+                    self.airScoreCircle.isHidden = false
+                    self.airScoreLabel.text = String(describing: airQuality!)
+                    self.cityLabel.isHidden = false
+                    self.cityLabel.text = self.cityNameTapped
+                    self.messageLabel.text = "The air’s pretty polluted. Better stick to the treadmill, and avoid going out!"
+                    self.trailImageView.isHidden = true
+                    self.treadmillImageView.isHidden = false
+                    self.airScoreCircle.color = UIColor(red: 0.651, green: 0.5059, blue: 0.7569, alpha: 1.0)
+                    self.airScoreCircle.setNeedsDisplay()
+                }
+            } else if airQuality! >= 300 {
+                print("Hazardous: brown")
+                
+                DispatchQueue.main.async {
+                    self.airScoreCircle.isHidden = false
+                    self.airScoreLabel.text = String(describing: airQuality!)
+                    self.cityLabel.isHidden = false
+                    self.cityLabel.text = self.cityNameTapped
+                    self.messageLabel.text = "The air is dangerously polluted. Better stick to the treadmill, and keep your windows shut!"
+                    self.trailImageView.isHidden = true
+                    self.treadmillImageView.isHidden = false
+                    self.airScoreCircle.color = UIColor(red: 0.6196, green: 0.5333, blue: 0.4118, alpha: 1.0)
+                    self.airScoreCircle.setNeedsDisplay()
                 }
             }
         }
--- Runners' Air Check/SearchViewController.swift
+++ Runners' Air Check/SearchViewController.swift
@@ -83,117 +83,112 @@ class SearchViewController: UIViewController, UISearchBarDelegate {
                 return
             }
             
-            if airQuality != nil {
-                
-                print(airQuality ?? "")
-                
-                if airQuality! < 51 {
-                    print("Good: green.")
-                    
-                    DispatchQueue.main.async {
-                        self.airScoreCircle.isHidden = false
-                        self.airScoreLabel.text = String(describing: airQuality!)
-                        self.cityLabel.isHidden = false
-                        self.cityLabel.text = self.cityName
-                        self.messageLabel.text = "The air’s great! \nGo run your socks off!!"
-                        self.dogImageView.isHidden = true
-                        self.treadmillImageView.isHidden = true
-                        self.trailImageView.isHidden = false
-                        self.saveButton.isHidden = false
-                        self.airScoreCircle.color = UIColor(red: 0.6824, green: 1, blue: 0.4863, alpha: 1.0)
-                        self.airScoreCircle.setNeedsDisplay()
-                        self.saveButton.layer.borderWidth = 1
-                        self.saveButton.layer.borderColor = UIColor.white.cgColor
-                    }
-                } else if airQuality! < 100 {
-                    print("Moderate: yellow")
-                    
-                    DispatchQueue.main.async {
-                        self.airScoreCircle.isHidden = false
-                        self.airScoreLabel.text = String(describing: airQuality!)
-                        self.cityLabel.isHidden = false
-                        self.cityLabel.text = self.cityName
-                        self.messageLabel.text = "The air’s pretty good. \nGet in those miles!"
-                        self.dogImageView.isHidden = true
-                        self.treadmillImageView.isHidden = true
-                        self.trailImageView.isHidden = false
-                        self.saveButton.isHidden = false
-                        self.airScoreCircle.color = UIColor(red: 1, green: 0.9647, blue: 0.4863, alpha: 1.0)
-                        self.airScoreCircle.setNeedsDisplay()
-                    }
-                } else if airQuality! < 150 {
-                    print("Unhealthy for sensitive groups: orange")
-                    
-                    DispatchQueue.main.async {
-                        self.airScoreCircle.isHidden = false
-                        self.airScoreLabel.text = String(describing: airQuality!)
-                        self.cityLabel.isHidden = false
-                        self.cityLabel.text = self.cityName
-                        self.messageLabel.text = "The air’s okay. Take it easy out there, especially if you have asthma!"
-                        self.dogImageView.isHidden = true
-                        self.treadmillImageView.isHidden = true
-                        self.trailImageView.isHidden = false
-                        self.saveButton.isHidden = false
-                        self.airScoreCircle.color = UIColor(red: 1, green: 0.7843, blue: 0.4863, alpha: 1.0)
-                        self.airScoreCircle.setNeedsDisplay()
-                    }
-                } else if airQuality! < 200 {
-                    print("Unhealthy: red")
-                    
-                    DispatchQueue.main.async {
-                        self.airScoreCircle.isHidden = false
-                        self.airScoreLabel.text = String(describing: airQuality!)
-                        self.cityLabel.isHidden = false
-                        self.cityLabel.text = self.cityName
-                        self.messageLabel.text = "The air’s not great. Better stick to the treadmill!"
-                        self.dogImageView.isHidden = true
-                        self.trailImageView.isHidden = true
-                        self.treadmillImageView.isHidden = false
-                        self.saveButton.isHidden = false
-                        self.airScoreCircle.color = UIColor(red: 0.9569, green: 0.4667, blue: 0.4667, alpha: 1.0)
-                        self.airScoreCircle.setNeedsDisplay()
-                    }
-                } else if airQuality! < 300 {
-                    print("Very unhealthy: purple")
-                    
-                    DispatchQueue.main.async {
-                        self.airScoreCircle.isHidden = false
-                        self.airScoreLabel.text = String(describing: airQuality!)
-                        self.cityLabel.isHidden = false
-                        self.cityLabel.text = self.cityName
-                        self.messageLabel.text = "The air’s pretty polluted. Better stick to the treadmill, and avoid going out!"
-                        self.dogImageView.isHidden = true
-                        self.trailImageView.isHidden = true
-                        self.treadmillImageView.isHidden = false
-                        self.saveButton.isHidden = false
-                        self.airScoreCircle.color = UIColor(red: 0.651, green: 0.5059, blue: 0.7569, alpha: 1.0)
-                        self.airScoreCircle.setNeedsDisplay()
-                    }
-                } else if airQuality! >= 300 {
-                    print("Hazardous: brown")
-                    
-                    DispatchQueue.main.async {
-                        self.airScoreCircle.isHidden = false
-                        self.airScoreLabel.text = String(describing: airQuality!)
-                        self.cityLabel.isHidden = false
-                        self.cityLabel.text = self.cityName
-                        self.messageLabel.text = "The air is dangerously polluted. Better stick to the treadmill, and keep your windows shut!"
-                        self.dogImageView.isHidden = true
-                        self.trailImageView.isHidden = true
-                        self.treadmillImageView.isHidden = false
-                        self.saveButton.isHidden = false
-                        self.airScoreCircle.color = UIColor(red: 0.6196, green: 0.5333, blue: 0.4118, alpha: 1.0)
-                        self.airScoreCircle.setNeedsDisplay()
-                    }
-                }
-                
-            } else {
-                
+            guard (airQuality != nil) else {
                 DispatchQueue.main.async {
                     self.airScoreCircle.isHidden = true
                     self.cityLabel.isHidden = true
-                    self.dogImageView.isHidden = false
                     self.messageLabel.text = "Sorry, there is currently no data for \(self.cityName!). Go for a run anyway!"
+                }
+                return
+            }
+                
+            if airQuality! < 51 {
+                print("Good: green.")
+                
+                DispatchQueue.main.async {
+                    self.airScoreCircle.isHidden = false
+                    self.airScoreLabel.text = String(describing: airQuality!)
+                    self.cityLabel.isHidden = false
+                    self.cityLabel.text = self.cityName
+                    self.messageLabel.text = "The air’s great! \nGo run your socks off!!"
+                    self.dogImageView.isHidden = true
+                    self.treadmillImageView.isHidden = true
+                    self.trailImageView.isHidden = false
+                    self.saveButton.isHidden = false
+                    self.airScoreCircle.color = UIColor(red: 0.6824, green: 1, blue: 0.4863, alpha: 1.0)
+                    self.airScoreCircle.setNeedsDisplay()
+                    self.saveButton.layer.borderWidth = 1
+                    self.saveButton.layer.borderColor = UIColor.white.cgColor
+                }
+            } else if airQuality! < 100 {
+                print("Moderate: yellow")
+                
+                DispatchQueue.main.async {
+                    self.airScoreCircle.isHidden = false
+                    self.airScoreLabel.text = String(describing: airQuality!)
+                    self.cityLabel.isHidden = false
+                    self.cityLabel.text = self.cityName
+                    self.messageLabel.text = "The air’s pretty good. \nGet in those miles!"
+                    self.dogImageView.isHidden = true
+                    self.treadmillImageView.isHidden = true
+                    self.trailImageView.isHidden = false
+                    self.saveButton.isHidden = false
+                    self.airScoreCircle.color = UIColor(red: 1, green: 0.9647, blue: 0.4863, alpha: 1.0)
+                    self.airScoreCircle.setNeedsDisplay()
+                }
+            } else if airQuality! < 150 {
+                print("Unhealthy for sensitive groups: orange")
+                
+                DispatchQueue.main.async {
+                    self.airScoreCircle.isHidden = false
+                    self.airScoreLabel.text = String(describing: airQuality!)
+                    self.cityLabel.isHidden = false
+                    self.cityLabel.text = self.cityName
+                    self.messageLabel.text = "The air’s okay. Take it easy out there, especially if you have asthma!"
+                    self.dogImageView.isHidden = true
+                    self.treadmillImageView.isHidden = true
+                    self.trailImageView.isHidden = false
+                    self.saveButton.isHidden = false
+                    self.airScoreCircle.color = UIColor(red: 1, green: 0.7843, blue: 0.4863, alpha: 1.0)
+                    self.airScoreCircle.setNeedsDisplay()
+                }
+            } else if airQuality! < 200 {
+                print("Unhealthy: red")
+                
+                DispatchQueue.main.async {
+                    self.airScoreCircle.isHidden = false
+                    self.airScoreLabel.text = String(describing: airQuality!)
+                    self.cityLabel.isHidden = false
+                    self.cityLabel.text = self.cityName
+                    self.messageLabel.text = "The air’s not great. Better stick to the treadmill!"
+                    self.dogImageView.isHidden = true
+                    self.trailImageView.isHidden = true
+                    self.treadmillImageView.isHidden = false
+                    self.saveButton.isHidden = false
+                    self.airScoreCircle.color = UIColor(red: 0.9569, green: 0.4667, blue: 0.4667, alpha: 1.0)
+                    self.airScoreCircle.setNeedsDisplay()
+                }
+            } else if airQuality! < 300 {
+                print("Very unhealthy: purple")
+                
+                DispatchQueue.main.async {
+                    self.airScoreCircle.isHidden = false
+                    self.airScoreLabel.text = String(describing: airQuality!)
+                    self.cityLabel.isHidden = false
+                    self.cityLabel.text = self.cityName
+                    self.messageLabel.text = "The air’s pretty polluted. Better stick to the treadmill, and avoid going out!"
+                    self.dogImageView.isHidden = true
+                    self.trailImageView.isHidden = true
+                    self.treadmillImageView.isHidden = false
+                    self.saveButton.isHidden = false
+                    self.airScoreCircle.color = UIColor(red: 0.651, green: 0.5059, blue: 0.7569, alpha: 1.0)
+                    self.airScoreCircle.setNeedsDisplay()
+                }
+            } else if airQuality! >= 300 {
+                print("Hazardous: brown")
+                
+                DispatchQueue.main.async {
+                    self.airScoreCircle.isHidden = false
+                    self.airScoreLabel.text = String(describing: airQuality!)
+                    self.cityLabel.isHidden = false
+                    self.cityLabel.text = self.cityName
+                    self.messageLabel.text = "The air is dangerously polluted. Better stick to the treadmill, and keep your windows shut!"
+                    self.dogImageView.isHidden = true
+                    self.trailImageView.isHidden = true
+                    self.treadmillImageView.isHidden = false
+                    self.saveButton.isHidden = false
+                    self.airScoreCircle.color = UIColor(red: 0.6196, green: 0.5333, blue: 0.4118, alpha: 1.0)
+                    self.airScoreCircle.setNeedsDisplay()
                 }
             }
         }
