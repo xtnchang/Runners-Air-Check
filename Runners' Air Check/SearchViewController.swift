d78983ded97953bec2d49d6242c22ed8072b2103
Christine Chang <xtnchang@gmail.com>
2017-11-06 12:43:43 -0800

    Fix switch statement formatting (indents)

--- Runners' Air Check/SearchViewController.swift
+++ Runners' Air Check/SearchViewController.swift
@@ -110,74 +110,74 @@ class SearchViewController: UIViewController, UISearchBarDelegate {
                 
                 case _ where airQuality! < 51:
                 
-                print("Good: green.")
-                
-                DispatchQueue.main.async {
-                
-                self.loadUI(airQuality: airQuality!, messageText: "The air’s great! \nGo run your socks off!!")
-                self.trailImageView.isHidden = false
-                self.airScoreCircle.color = UIColor(red: 0.6824, green: 1, blue: 0.4863, alpha: 1.0)
-                self.airScoreCircle.setNeedsDisplay()
-                
+                    print("Good: green.")
+                    
+                    DispatchQueue.main.async {
+                    
+                    self.loadUI(airQuality: airQuality!, messageText: "The air’s great! \nGo run your socks off!!")
+                    self.trailImageView.isHidden = false
+                    self.airScoreCircle.color = UIColor(red: 0.6824, green: 1, blue: 0.4863, alpha: 1.0)
+                    self.airScoreCircle.setNeedsDisplay()
                 }
+                
                 case _ where airQuality! >= 51 && airQuality! < 100:
                 
-                print("Moderate: yellow")
-                
-                DispatchQueue.main.async {
-                
-                self.loadUI(airQuality: airQuality!, messageText: "The air’s pretty good. \nGet in those miles!")
-                self.trailImageView.isHidden = false
-                self.airScoreCircle.color = UIColor(red: 1, green: 0.9647, blue: 0.4863, alpha: 1.0)
-                self.airScoreCircle.setNeedsDisplay()
+                    print("Moderate: yellow")
+                    
+                    DispatchQueue.main.async {
+                    
+                    self.loadUI(airQuality: airQuality!, messageText: "The air’s pretty good. \nGet in those miles!")
+                    self.trailImageView.isHidden = false
+                    self.airScoreCircle.color = UIColor(red: 1, green: 0.9647, blue: 0.4863, alpha: 1.0)
+                    self.airScoreCircle.setNeedsDisplay()
                 }
                 
                 case _ where airQuality! >= 100 && airQuality! < 150:
                 
-                print("Unhealthy for sensitive groups: orange")
-                
-                DispatchQueue.main.async {
-                
-                self.loadUI(airQuality: airQuality!, messageText: "The air’s okay. Take it easy out there, especially if you have asthma!")
-                self.trailImageView.isHidden = false
-                self.airScoreCircle.color = UIColor(red: 1, green: 0.7843, blue: 0.4863, alpha: 1.0)
-                self.airScoreCircle.setNeedsDisplay()
+                    print("Unhealthy for sensitive groups: orange")
+                    
+                    DispatchQueue.main.async {
+                    
+                    self.loadUI(airQuality: airQuality!, messageText: "The air’s okay. Take it easy out there, especially if you have asthma!")
+                    self.trailImageView.isHidden = false
+                    self.airScoreCircle.color = UIColor(red: 1, green: 0.7843, blue: 0.4863, alpha: 1.0)
+                    self.airScoreCircle.setNeedsDisplay()
                 }
                 
                 case _ where airQuality! >= 150 && airQuality! < 200:
                 
-                print("Unhealthy: red")
-                
-                DispatchQueue.main.async {
-                
-                self.loadUI(airQuality: airQuality!, messageText: "The air’s not great. Better stick to the treadmill!")
-                self.treadmillImageView.isHidden = false
-                self.airScoreCircle.color = UIColor(red: 0.9569, green: 0.4667, blue: 0.4667, alpha: 1.0)
-                self.airScoreCircle.setNeedsDisplay()
+                    print("Unhealthy: red")
+                    
+                    DispatchQueue.main.async {
+                    
+                    self.loadUI(airQuality: airQuality!, messageText: "The air’s not great. Better stick to the treadmill!")
+                    self.treadmillImageView.isHidden = false
+                    self.airScoreCircle.color = UIColor(red: 0.9569, green: 0.4667, blue: 0.4667, alpha: 1.0)
+                    self.airScoreCircle.setNeedsDisplay()
                 }
                 
                 case _ where airQuality! >= 200 && airQuality! < 300:
                 
-                print("Very unhealthy: purple")
-                
-                DispatchQueue.main.async {
-                
-                self.loadUI(airQuality: airQuality!, messageText: "The air’s pretty polluted. Better stick to the treadmill, and avoid going out!")
-                self.treadmillImageView.isHidden = false
-                self.airScoreCircle.color = UIColor(red: 0.651, green: 0.5059, blue: 0.7569, alpha: 1.0)
-                self.airScoreCircle.setNeedsDisplay()
+                    print("Very unhealthy: purple")
+                    
+                    DispatchQueue.main.async {
+                    
+                    self.loadUI(airQuality: airQuality!, messageText: "The air’s pretty polluted. Better stick to the treadmill, and avoid going out!")
+                    self.treadmillImageView.isHidden = false
+                    self.airScoreCircle.color = UIColor(red: 0.651, green: 0.5059, blue: 0.7569, alpha: 1.0)
+                    self.airScoreCircle.setNeedsDisplay()
                 }
                 
                 case _ where airQuality! >= 300:
                 
-                print("Hazardous: brown")
-                
-                DispatchQueue.main.async {
-                
-                self.loadUI(airQuality: airQuality!, messageText: "The air is dangerously polluted. Better stick to the treadmill, and keep your windows shut!")
-                self.treadmillImageView.isHidden = false
-                self.airScoreCircle.color = UIColor(red: 0.6196, green: 0.5333, blue: 0.4118, alpha: 1.0)
-                self.airScoreCircle.setNeedsDisplay()
+                    print("Hazardous: brown")
+                    
+                    DispatchQueue.main.async {
+                    
+                    self.loadUI(airQuality: airQuality!, messageText: "The air is dangerously polluted. Better stick to the treadmill, and keep your windows shut!")
+                    self.treadmillImageView.isHidden = false
+                    self.airScoreCircle.color = UIColor(red: 0.6196, green: 0.5333, blue: 0.4118, alpha: 1.0)
+                    self.airScoreCircle.setNeedsDisplay()
                 }
                 
                 default:
